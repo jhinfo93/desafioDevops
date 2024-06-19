21d4c81ce6eb5bc27874ba5dc8b3c87f69bc4889
Jackson Henrique <jacksonh95@gmail.com>
2024-06-19 14:32:28 -0300

    deploy 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Bem-vindo à Página de Teste 1!
+          Bem-vindo à Página de Teste 2!
         </h1>
         <p className={styles.description}>
           Data e hora atual: {new Date().toLocaleString()}
